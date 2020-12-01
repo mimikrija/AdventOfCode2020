075552a029c92ca974d892a081534b4989ade456
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 13:11:52 +0100

    [day01] fix spacing

--- 01.py
+++ 01.py
@@ -3,8 +3,8 @@ with open('inputs/01') as inputfile:
 
 inputs = [ int(line.strip()) for line in inputs ]
 
-part1 = { num1+num2 : num1 * num2 for num1 in inputs for num2 in inputs }
-part2 = { num1+num2+num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
+part1 = { num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs }
+part2 = { num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
 
 
 print(f'Part1 solution is: {part1[2020]}!')
