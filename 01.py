fc7213a3b918b686180b4c75e0f4a78ee0d19006
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 13:29:08 +0100

    [day01] add solutions

--- 01.py
+++ 01.py
@@ -6,6 +6,7 @@ inputs = [ int(line.strip()) for line in inputs ]
 part1 = { num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs }
 part2 = { num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
 
-
-print(f'Part1 solution is: {part1[2020]}!')
-print(f'Part2 solution is: {part2[2020]}!')
+print(f'Part 1 solution is: {part1[2020]}!')
+print(f'Part 2 solution is: {part2[2020]}!')
+# Part 1 solution is: 388075!
+# Part 2 solution is: 293450526!
