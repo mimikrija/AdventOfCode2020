5f4ff45e8b5c11ad391f06b2c7148ea1a439f5c8
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 19:06:45 +0100

    [day01] remove spacing before brackets and braces

--- 01.py
+++ 01.py
@@ -1,10 +1,10 @@
 with open('inputs/01') as inputfile:
     inputs = inputfile.readlines()
 
-inputs = [ int(line.strip()) for line in inputs ]
+inputs = [int(line.strip()) for line in inputs]
 
-part1 = { num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs }
-part2 = { num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
+part1 = {num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs}
+part2 = {num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
 
 print(f'Part 1 solution is: {part1[2020]}!')
 print(f'Part 2 solution is: {part2[2020]}!')
