484eea9d288dbe939cbfdbe1622582fa13971535
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 06:50:51 +0100

    [day01] part 2, same thing but tripple

--- 01.py
+++ 01.py
@@ -3,8 +3,9 @@ with open('inputs/01') as inputfile:
 
 inputs = [ int(line.strip()) for line in inputs ]
 
-sums1 = { num1+num2 : num1 * num2 for num1 in inputs for num2 in inputs }
+part1 = { num1+num2 : num1 * num2 for num1 in inputs for num2 in inputs }
+part2 = { num1+num2+num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
 
 
-
-print(f{'Part 1 solution is: {sums1[2020]}')
+print(f'Part1 solution is: {part1[2020]}!')
+print(f'Part2 solution is: {part2[2020]}!')
