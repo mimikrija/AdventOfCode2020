bef9b915977b4fe481bf2b637a8602f48d3ac5de
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 19:14:10 +0100

    [day01][TIL] enter `enumerate`!!
    Today I learned that instead of using indices, there is a much nicer,
    built-in way of keeping track of collection iterators (if that's what's
    actually needed, and in this puzzle it was).
    
    The enumerate() method adds counter to the iterable. The returned object
    is an enumerate object.
    
    Read more [here](https://www.tutorialspoint.com/enumerate-in-python)

--- 01.py
+++ 01.py
@@ -3,8 +3,14 @@ with open('inputs/01') as inputfile:
 
 inputs = [int(line.strip()) for line in inputs]
 
-part1 = {num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs[inputs.index(num1)+1:]}
-part2 = {num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs[inputs.index(num1)+1:] for num3 in inputs[inputs.index(num2)+1:]}
+part1 = {num1 + num2 : num1 * num2
+        for i, num1 in enumerate(inputs)
+        for num2 in inputs[i+1:]}
+
+part2 = {num1 + num2 + num3 : num1 * num2 * num3
+        for i, num1 in enumerate(inputs)
+        for j, num2 in enumerate(inputs[i+1:])
+        for num3 in inputs[j+1:]}
 
 print(f'Part 1 solution is: {part1[2020]}!')
 print(f'Part 2 solution is: {part2[2020]}!')
