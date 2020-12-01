f16c6f92653c3da20158241d85cfba2cfe0e1aee
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-01 19:09:20 +0100

    [day01] skip already used nums to speeds things up
    not only that, but with this fix, if the input contains number 1010, it
    will still return a correct solution, as it doesn't loop twice through
    all the numbers.

--- 01.py
+++ 01.py
@@ -3,8 +3,8 @@ with open('inputs/01') as inputfile:
 
 inputs = [int(line.strip()) for line in inputs]
 
-part1 = {num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs}
-part2 = {num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs for num3 in inputs}
+part1 = {num1 + num2 : num1 * num2 for num1 in inputs for num2 in inputs[inputs.index(num1)+1:]}
+part2 = {num1 + num2 + num3 : num1 * num2 * num3 for num1 in inputs for num2 in inputs[inputs.index(num1)+1:] for num3 in inputs[inputs.index(num2)+1:]}
 
 print(f'Part 1 solution is: {part1[2020]}!')
 print(f'Part 2 solution is: {part2[2020]}!')
