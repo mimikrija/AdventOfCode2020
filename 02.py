7072fd36f19b78ffa5d5be96a1655737aa8c85f5
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-02 08:13:08 +0100

    [day02] adjust wording a bit

--- 02.py
+++ 02.py
@@ -29,6 +29,6 @@ for line in inputs:
     part_2 += is_password2(rule)
 
 print(f'The number of valid passwords is {part_1}!')
-print(f'The number of valid passwords is {part_2}, following the second rule!')
+print(f'The number of valid passwords is {part_2} (following the second rule)!')
 # The number of valid passwords is 536!
-# The number of valid passwords is 558, following the second rule!
+# The number of valid passwords is 558 (following the second rule)!
