812533f68ef9638aa3c924c70572c68d9dc97b1d
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-02 06:29:20 +0100

    [day02] part 1 first go

--- 02.py
+++ 02.py
@@ -1,6 +1,13 @@
 import re
 
-with open('inputs/02-ex') as inputfile:
+def is_password(input):
+    min_count = int(input[0])
+    max_count = int(input[1])
+    character = input[2]
+    password = input[3]
+    return password.count(character) >= min_count and password.count(character) <= max_count
+
+with open('inputs/02') as inputfile:
     inputs = inputfile.readlines()
 
 words =re.compile(r'\w+')
@@ -9,3 +16,8 @@ rules = []
 for line in inputs:
     rules.append(re.findall(words,line))
 
+solution1 = 0
+for rule in rules:
+    solution1 += is_password(rule)
+
+print(solution1)
