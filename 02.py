63124ddaa7517b1fa2a81dbd67a64581266349be
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-02 20:34:59 +0100

    [day02] fix spacing and naming

--- 02.py
+++ 02.py
@@ -5,10 +5,10 @@ def parse(unparsed_rule):
     first, second = int(first), int(second)
     return([first, second, char, word])
 
-def is_password1(min_count, max_count, character, password):
+def is_password_1(min_count, max_count, character, password):
     return max_count >= password.count(character) >= min_count
 
-def is_password2(first_char, second_char, character, password):
+def is_password_2(first_char, second_char, character, password):
     return (password[first_char-1] == character) ^ (password[second_char-1] == character)
 
 
@@ -23,8 +23,8 @@ part_2 = 0
 for line in inputs:
     rule = re.findall(words, line)
     rule = parse(rule)
-    part_1 += is_password1(*rule)
-    part_2 += is_password2(*rule)
+    part_1 += is_password_1(*rule)
+    part_2 += is_password_2(*rule)
 
 print(f'The number of valid passwords is {part_1}!')
 print(f'The number of valid passwords is {part_2} (following the second rule)!')
