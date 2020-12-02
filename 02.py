923572c643b8ccf25486efc3403b98245f6c2c4b
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-02 20:31:30 +0100

    [day02][TIL] parse input once only
    Today I learned and implemented:
    When calling a function with multiple args, you can call it with
    `*tupple` where that tupple contains the number of args that that
    function takes. So the asterisk symbol unpacks the tupple. Neat!

--- 02.py
+++ 02.py
@@ -3,14 +3,12 @@ import re
 def parse(unparsed_rule):
     first, second, char, word = unparsed_rule
     first, second = int(first), int(second)
-    return(first, second, char, word)
+    return([first, second, char, word])
 
-def is_password1(input):
-    min_count, max_count, character, password = parse(input)
+def is_password1(min_count, max_count, character, password):
     return max_count >= password.count(character) >= min_count
 
-def is_password2(input):
-    first_char, second_char, character, password = parse(input)
+def is_password2(first_char, second_char, character, password):
     return (password[first_char-1] == character) ^ (password[second_char-1] == character)
 
 
@@ -24,8 +22,9 @@ part_2 = 0
 
 for line in inputs:
     rule = re.findall(words, line)
-    part_1 += is_password1(rule)
-    part_2 += is_password2(rule)
+    rule = parse(rule)
+    part_1 += is_password1(*rule)
+    part_2 += is_password2(*rule)
 
 print(f'The number of valid passwords is {part_1}!')
 print(f'The number of valid passwords is {part_2} (following the second rule)!')
