653bc1d011970a5a4e36989a961e786ccff04aa9
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-03 06:27:20 +0100

    [day03] part 1 first try

--- 03.py
+++ 03.py
@@ -1,5 +1,16 @@
+def is_tree(row, column_count):
+    return row[column_count%row_length] == "#"
+
 with open('inputs/03') as inputfile:
     inputs = inputfile.readlines()
 
 inputs = [row.strip() for row in inputs]
+row_length = len(inputs[0])
+
+part_1 = 0
+
+for column_pos, row in enumerate(inputs):
+    part_1 += is_tree(row, column_pos*3)
+
+print(part_1)
 
