1279ce0dadab1135ff1d6670d8b21b28e69809f5
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-03 09:54:53 +0100

    [day03] add function to count trees for various ratios

--- 03.py
+++ 03.py
@@ -1,6 +1,13 @@
 def is_tree(row, column_count):
     return row[column_count%row_length] == "#"
 
+def tree_count(rows, column_multiplier, row_multiplier):
+    count = 0
+    for row_n, row in enumerate(inputs):
+        if row_n%row_multiplier == 0:
+            count += is_tree(row, row_n*column_multiplier//row_multiplier)
+    return count
+
 with open('inputs/03') as inputfile:
     inputs = inputfile.readlines()
 
