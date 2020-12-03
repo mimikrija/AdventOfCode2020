2dccf2490bc72fb33ac8b628a120aa0aeb780020
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-03 07:31:43 +0100

    [day03] ugly part 2, clean this up

--- 03.py
+++ 03.py
@@ -8,9 +8,33 @@ inputs = [row.strip() for row in inputs]
 row_length = len(inputs[0])
 
 part_1 = 0
+part_2 = 1
+slopes = [1, 3, 5, 7]
+position_counter = 0
+
 
 for column_pos, row in enumerate(inputs):
     part_1 += is_tree(row, column_pos*3)
 
-print(part_1)
 
+for slope in slopes:
+    temp_2 = 0
+    for column_pos, row in enumerate(inputs):
+        temp_2 += is_tree(row, column_pos*slope)
+    if temp_2 > 0:
+        part_2 *= temp_2
+
+
+temp_2 = 0
+for column_pos, row in enumerate(inputs):
+    if column_pos%2 == 0:
+        temp_2 += is_tree(row, column_pos//2)
+
+if temp_2 > 0:
+    part_2 *= temp_2
+
+
+print(f'Part 1 solution is: {part_1}! {part_1 == 292}')
+print(f'Part 2 solution is: {part_2}! {part_2 == 9354744432}')
+# Part 1 solution is: 292!
+# Part 2 solution is: 9354744432!
