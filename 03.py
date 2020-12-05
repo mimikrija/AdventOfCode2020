38e31caabbc313bc12c950158f63e66f9ab663c9
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-05 15:30:52 +0100

    [day03] `rows` list of all rows in input/map

--- 03.py
+++ 03.py
@@ -4,21 +4,21 @@ def is_tree(row, column):
 
 def tree_count(rows, column_multiplier, row_multiplier):
     count = 0
-    for row_n, row in enumerate(inputs):
+    for row_n, row in enumerate(rows):
         if row_n%row_multiplier == 0:
             count += is_tree(row, row_n*column_multiplier//row_multiplier)
     return count
 
 with open('inputs/03') as inputfile:
-    inputs = inputfile.readlines()
+    rows = inputfile.readlines()
 
-inputs = [row.strip() for row in inputs]
-row_length = len(inputs[0])
+rows = [row.strip() for row in rows]
+row_length = len(rows[0])
 
 ratios_to_check = [(c,r) for r in range (1,3) for c in range (1,8,2) if r != 2 or c < r]
 # [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] (part 2 ratios to check)
 
-all_ratios_tree_count = [tree_count(inputs,*ratio) for ratio in ratios_to_check]
+all_ratios_tree_count = [tree_count(rows,*ratio) for ratio in ratios_to_check]
 
 part_1 = all_ratios_tree_count[1]
 part_2 = 1
