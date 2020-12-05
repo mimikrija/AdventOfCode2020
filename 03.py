32326a3f8a64591dba525d0427edb90bc6b44e80
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-05 18:05:26 +0100

    [day05] rename variables

--- 03.py
+++ 03.py
@@ -2,11 +2,12 @@ def is_tree(row, column):
     # if column is out of range, the map continues hence %
     return row[column%row_length] == "#"
 
-def tree_count(rows, column_multiplier, row_multiplier):
+def tree_count(rows, ratio):
+    delta_column, delta_row = ratio
     count = 0
     for row_n, row in enumerate(rows):
-        if row_n%row_multiplier == 0:
-            count += is_tree(row, row_n*column_multiplier//row_multiplier)
+        if row_n%delta_row == 0:
+            count += is_tree(row, (row_n*delta_column)//delta_row)
     return count
 
 with open('inputs/03') as inputfile:
@@ -18,7 +19,7 @@ row_length = len(rows[0])
 ratios_to_check = [(c,r) for r in range (1,3) for c in range (1,8,2) if r != 2 or c < r]
 # [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] (part 2 ratios to check)
 
-all_ratios_tree_count = [tree_count(rows,*ratio) for ratio in ratios_to_check]
+all_ratios_tree_count = [tree_count(rows, ratio) for ratio in ratios_to_check]
 
 part_1 = all_ratios_tree_count[1]
 part_2 = 1
