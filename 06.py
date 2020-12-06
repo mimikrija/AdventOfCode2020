2e575b0ceb4a29a7c1b388fcce1dfb7368c4179d
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-06 06:51:00 +0100

    [day06] part 2 first go

--- 06.py
+++ 06.py
@@ -2,12 +2,20 @@ import collections
 # read & parse input
 # 1. read data in bulks split by \n\n
 customs_input = open('inputs/06').read().split('\n\n')
-print(customs_input)
+
 part_1 = 0
+part_2 = 0
 for answers in customs_input:
     count = collections.Counter(answers)
+    people_in_group = count['\n']+1
     for letter, num in count.items():
         if letter != '\n':
             part_1 += 1
+        if num == people_in_group:
+            part_2 += 1
 
 print(part_1)
+# 6778
+
+print(part_2)
+# 3406
