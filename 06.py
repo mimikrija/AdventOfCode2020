9be7232360f4ea490b37d97efecda4b6b6e60c96
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-06 12:49:23 +0100

    [day06] clean up a bit

--- 06.py
+++ 06.py
@@ -1,18 +1,18 @@
-import collections
-# read & parse input
-# 1. read data in bulks split by \n\n
+from collections import Counter
+
+# read data in bulks split by \n\n :)
 customs_input = open('inputs/06').read().split('\n\n')
 
 part_1 = 0
 part_2 = 0
 for answers in customs_input:
-    count = collections.Counter(answers)
-    people_in_group = count['\n']+1
-    for letter, num in count.items():
-        if letter != '\n':
-            part_1 += 1
-        if num == people_in_group:
-            part_2 += 1
+    count_answers = Counter(answers)
+    # part 1: count all letters that appear in one bulk
+    part_1 += len(count_answers) - ('\n' in count_answers.keys())
+    # part 2: count only letters that appear in *every* line of bulk
+    people_in_group = count_answers['\n'] + 1
+    for _, num in count_answers.items():
+        part_2 += num == people_in_group
 
 print(part_1)
 # 6778
