11eda05a814239b259f01993c1295baf071805bd
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-07 06:43:17 +0100

    [day07] fix reading and parsing, ugh

--- 07.py
+++ 07.py
@@ -1,5 +1,9 @@
 with open('inputs/07-ex') as inputfile:
     inputs = inputfile.readlines()
 
+parents = []
+children = []
 for line in inputs:
-    parent_bags, children_bags = line.split('contain')
+    parent_bag, children_bag = line.strip().split(' bags contain ')
+    parents.append(parent_bag)
+    children.append(children_bag)
