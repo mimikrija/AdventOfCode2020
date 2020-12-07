94363194331668f84225207f3167e2ca49071fd6
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-07 07:33:37 +0100

    [day07] first working try part 1

--- 07.py
+++ 07.py
@@ -1,9 +1,19 @@
-with open('inputs/07-ex') as inputfile:
+with open('inputs/07') as inputfile:
     inputs = inputfile.readlines()
 
-parents = []
-children = []
-for line in inputs:
-    parent_bag, children_bag = line.strip().split(' bags contain ')
-    parents.append(parent_bag)
-    children.append(children_bag)
+everything = [line.strip() for line in inputs]
+
+
+find_next = ['shiny gold']
+
+part_1 = 0
+all_solutions = []
+def find_bags(find_next, all_solutions):
+    while find_next != []:
+        for bag in find_next:
+            find_next = [rule.split(' bags contain')[0] for rule in everything if bag in rule and rule.find(bag) != 0]
+            find_bags(find_next,all_solutions)
+            all_solutions += find_next
+    return len(set(all_solutions))
+
+print(find_bags(find_next,[])) #246
