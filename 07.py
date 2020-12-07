66318b34653ef67be8c89e3266b70ef5e17b25ea
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-07 07:46:16 +0100

    [day07] clean up part 1

--- 07.py
+++ 07.py
@@ -3,17 +3,15 @@ with open('inputs/07') as inputfile:
 
 everything = [line.strip() for line in inputs]
 
-
-find_next = ['shiny gold']
-
-part_1 = 0
-all_solutions = []
-def find_bags(find_next, all_solutions):
+def find_outter_bags(find_next, all_solutions):
     while find_next != []:
         for bag in find_next:
             find_next = [rule.split(' bags contain')[0] for rule in everything if bag in rule and rule.find(bag) != 0]
-            find_bags(find_next,all_solutions)
+            find_outter_bags(find_next,all_solutions)
             all_solutions += find_next
     return len(set(all_solutions))
 
-print(find_bags(find_next,[])) #246
+part_1 = find_outter_bags(['shiny gold'],[])
+
+print(f'{part_1} bag colors contain at least one shiny gold bag!')
+# 246 bag colors contain at least one shiny gold bag!
