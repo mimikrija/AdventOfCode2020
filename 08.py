fd5c1062f6025425f8ab6f588aa40d12f3e4591a
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-08 07:46:23 +0100

    [day08] part 2 first go - multiple solutions possible though

--- 08.py
+++ 08.py
@@ -18,9 +18,43 @@ current_acc = 0
 current_position = 0
 
 while current_position not in visited_positions:
+    if current_position == len(instructions):
+        print('success')
+        break
     visited_positions.append(current_position)
     action, argument = instructions[current_position]
     acc_offset, position_offset = do_stuff(action, argument)
     current_acc += acc_offset
     current_position += position_offset
 print(current_acc) # 1553
+
+for n, instruction in enumerate(instructions):
+    action, argument = instruction
+    if action == 'nop':
+        new_action = 'jmp'
+    if action == 'jmp':
+        new_action = 'nop'
+    new_instructions = list(instructions)
+    new_instructions[n] = (new_action, argument)
+    
+    visited_positions = []
+
+    current_acc = 0
+    current_position = 0
+
+    while current_position not in visited_positions:
+        if current_position == len(new_instructions):
+            print('success: ', current_acc)
+            break
+        visited_positions.append(current_position)
+        action, argument = new_instructions[current_position]
+        acc_offset, position_offset = do_stuff(action, argument)
+        current_acc += acc_offset
+        current_position += position_offset
+    #print(current_acc, current_position, current_position == len(new_instructions)) # 1553
+    if current_position == len(new_instructions):
+        print(current_acc)
+        break
+# success:  1877 this one
+# success:  505 not
+# success:  895
