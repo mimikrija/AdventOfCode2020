786fdedfb945c5606b7922cc5572baf65c8dbc23
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-08 07:59:56 +0100

    [day08] run prog from a function

--- 08.py
+++ 08.py
@@ -12,21 +12,24 @@ def do_stuff(action, argument):
     if action == 'jmp':
         return 0, argument
 
-visited_positions = []
+def run_program(instructions):
+    visited_positions = []
 
-current_acc = 0
-current_position = 0
+    current_acc = 0
+    current_position = 0
 
-while current_position not in visited_positions:
-    if current_position == len(instructions):
-        print('success')
-        break
-    visited_positions.append(current_position)
-    action, argument = instructions[current_position]
-    acc_offset, position_offset = do_stuff(action, argument)
-    current_acc += acc_offset
-    current_position += position_offset
-print(current_acc) # 1553
+    while current_position not in visited_positions:
+        if current_position == len(instructions):
+            print('success')
+            break
+        visited_positions.append(current_position)
+        action, argument = instructions[current_position]
+        acc_offset, position_offset = do_stuff(action, argument)
+        current_acc += acc_offset
+        current_position += position_offset
+    return(current_acc, current_position) # 1553
+
+print(run_program(instructions))
 
 for n, instruction in enumerate(instructions):
     action, argument = instruction
@@ -36,25 +39,10 @@ for n, instruction in enumerate(instructions):
         new_action = 'nop'
     new_instructions = list(instructions)
     new_instructions[n] = (new_action, argument)
-    
-    visited_positions = []
-
-    current_acc = 0
-    current_position = 0
-
-    while current_position not in visited_positions:
-        if current_position == len(new_instructions):
-            print('success: ', current_acc)
-            break
-        visited_positions.append(current_position)
-        action, argument = new_instructions[current_position]
-        acc_offset, position_offset = do_stuff(action, argument)
-        current_acc += acc_offset
-        current_position += position_offset
-    #print(current_acc, current_position, current_position == len(new_instructions)) # 1553
+    current_acc, current_position = run_program(new_instructions)
     if current_position == len(new_instructions):
         print(current_acc)
         break
 # success:  1877 this one
 # success:  505 not
-# success:  895
+# success:  895 not
