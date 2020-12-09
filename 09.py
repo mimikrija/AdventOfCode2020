c7975bc52b5fc2bd8d939c506e75dee3b1a09f51
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-09 13:08:18 +0100

    [day09] testing yield

--- 09.py
+++ 09.py
@@ -11,6 +11,12 @@ def is_sum(check_list, number):
                 return True
     return False
 
+def yield_func(l):
+    total = 0
+    for n in l:
+        yield total
+        total += n
+
 start = 25
 step = start
 
@@ -19,19 +25,26 @@ for position in range(start, len(inputs)):
     if not(is_sum(check_in, inputs[position])):
         part_1 = inputs[position]
         break
+import time
+a = time.time()*1000
 
 for position in range (0, len(inputs)):
-    for size in range (3, len(inputs)):
-        # create a sublist of size 2+
-        check_in = inputs[position:size]
-        # skip further creation of sublists:
-        if sum(check_in) >= part_1:
+    test = 0
+    counter = position
+    new_list = yield_func(inputs[position:])
+    while test <= part_1:
+        test = next(new_list)
+        counter += 1 
+        if test == part_1:
             break
-    if sum(check_in) == part_1:
-        part_2 = min(check_in) + max(check_in)
+    if test == part_1:
+        print(position, test, counter, min(inputs[position:counter])+max(inputs[position:counter]))
         break
+b = time.time()*1000
+print(b-a)
+
 
 print(f'The first number that is not the sum of any two of the 25 numbers before it is {part_1}!')
-print(f'The encryption weakness in my XMAS-encrypted list is: {part_2}!')
+#print(f'The encryption weakness in my XMAS-encrypted list is: {part_2}!')
 # The first number that is not the sum of any two of the 25 numbers before it is 32321523!
 # The encryption weakness in my XMAS-encrypted list is: 4794981!
