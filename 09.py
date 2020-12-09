682399ad127d7232873891c7236a50706134c599
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-09 06:27:50 +0100

    [day09] part 1 first go

---
+++ 09.py
@@ -0,0 +1,25 @@
+with open('inputs/09') as inputfile:
+    inputs = inputfile.readlines()
+
+inputs = [int(line.strip()) for line in inputs]
+
+
+start = 25
+end = len(inputs)-1
+step = start
+part_1 = 0
+
+def is_sum(check_list, number):
+    for i, num1 in enumerate(check_list):
+        for num2 in check_list[i+1:]:
+            if num1 + num2 == number and num1 != num2:
+                return True
+    return False
+
+for position in range (start,len(inputs)):
+    check_in = inputs[position-step:position]
+    if not(is_sum(check_in, inputs[position])):
+        part_1 = inputs[position]
+        break
+
+print(part_1) # 32321523
