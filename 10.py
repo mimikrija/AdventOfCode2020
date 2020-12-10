afc214807f9612f593ff58ab61589cc18e8b78a7
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-10 14:29:37 +0100

    [day10] part 2 first go (assumptions below)
    I've counted the sublists for a larger range and figured out that I
    only need three possible combination counts. Not sure if this would
    work with any input. I will try to generalize it a bit later.

--- 10.py
+++ 10.py
@@ -5,9 +5,29 @@ with open('inputs/10') as inputfile:
 inputs = [int(line.strip()) for line in inputs]
 
 inputs.sort()
+inputs = [0] + inputs + [max(inputs) + 3]
 
 diffs = [j-i for i,j in zip(inputs[:-1],inputs[1:])]
 
 counted_diffs = Counter(diffs)
 
-print((counted_diffs[1]+1)*(counted_diffs[3]+1))
+print((counted_diffs[1])*(counted_diffs[3]))
+
+test_list = [3] + diffs
+counted_ones = []
+for i in range(1,4):
+    sublist = [3] + (i+1)*[1] + [3]
+    count_sublist = len([sublist for idx in range(len(test_list)) if test_list[idx : idx + len(sublist)] == sublist])
+    counted_ones.append((i, count_sublist))
+
+combinations = {
+    1: 2,
+    2: 4,
+    3: 7
+}
+
+part_2 = 1
+for n, c in counted_ones:
+    part_2 *= combinations[n]**c
+
+print(part_2) # 226775649501184
