d321d0120006826e0f992531105206abb15a8a81
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-11 20:01:22 +0100

    [day11] lazy visible coordinates def

--- 11.py
+++ 11.py
@@ -7,6 +7,67 @@ def neighbours(input_configuration, position):
         if (check_row != row_pos or check_seat != seat_pos)
         and (check_row, check_seat) in input_configuration.keys()]
 
+def visible_seats(input_configuration, position):
+    visible = []
+    row_pos, seat_pos = position
+    # up
+    for up in range(row_pos - 1, -1, -1):
+        check_position = (up, seat_pos)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # left
+    for up in range(seat_pos - 1, -1, -1):
+        check_position = (row_pos, up)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # down
+    for up in range(row_pos + 1, total_rows):
+        check_position = (up, seat_pos)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # right
+    for up in range(seat_pos + 1, total_seats):
+        check_position = (row_pos, up)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+    
+    # up left diagonal
+    for x, y in zip( range(row_pos - 1, -1, -1), range(seat_pos - 1, -1, -1)):
+        check_position = (x, y)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # up right diagonal
+    for x, y in zip( range(row_pos - 1, -1, -1), range(seat_pos + 1, total_seats)):
+        check_position = (x, y)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # down left diagonal
+    for x, y in zip( range(row_pos + 1, total_rows), range(seat_pos - 1, -1, -1)):
+        check_position = (x, y)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    # down right diagonal
+    for x, y in zip( range(row_pos + 1, total_rows), range(seat_pos + 1, total_seats)):
+        check_position = (x, y)
+        if check_position in input_configuration.keys():
+            visible.append(check_position)
+            break
+
+    return visible
+
 def is_seat_desirable(input_configuration, position):
     if input_configuration[position] != 'L':
         return False
@@ -48,6 +109,9 @@ for row in rows:
     row = [c for c in row]
     seat_configuration.append(row)
 
+# get input dimensions
+total_rows = len(seat_configuration)
+total_seats = len(row)
 
 all_coordinates = {}
 for row_c, row in enumerate(seat_configuration):
