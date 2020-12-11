902f33b3b78be6fb3fdcd42453ac733c282d93c0
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-11 18:52:57 +0100

    [day11] ignore non existent coords in neighbours

--- 11.py
+++ 11.py
@@ -1,32 +1,30 @@
 import copy
 
-def neighbours(position):
+def neighbours(input_configuration, position):
     row_pos, seat_pos = position
     return [(check_row, check_seat)
         for check_row in range (row_pos-1, row_pos + 2) for check_seat in range (seat_pos-1, seat_pos+2)
         if (check_row != row_pos or check_seat != seat_pos)
-        and 0 <= check_row < total_rows and 0 <= check_seat < total_seats]
+        and (check_row, check_seat) in input_configuration.keys()]
 
 def is_seat_desirable(input_configuration, position):
     if input_configuration[position] != 'L':
         return False
     count = 0
     checked_seats = 0
-    for check_position in neighbours(position):
-        if check_position in input_configuration:
-            checked_seats += 1
-            count += input_configuration[check_position] == 'L'
+    for check_position in neighbours(input_configuration, position):
+        checked_seats += 1
+        count += input_configuration[check_position] == 'L'
     return checked_seats == count
 
 def is_seat_undesirable(input_configuration, position):
     if input_configuration[position] != '#':
         return False
     count = 0
-    for check_position in neighbours(position):
-        if check_position in input_configuration:
-            count += input_configuration[check_position] == '#'
-        if count >= 4:
-            return True
+    for check_position in neighbours(input_configuration, position):
+        count += input_configuration[check_position] == '#'
+    if count >= 4:
+        return True
     return False
 
 def update_configuration(input_configuration):
@@ -49,10 +47,6 @@ for row in rows:
     row = row.strip()
     row = [c for c in row]
     seat_configuration.append(row)
-
-# get input dimensions
-total_rows = len(seat_configuration)
-total_seats = len(row)
 
 
 all_coordinates = {}
