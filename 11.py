7b9c428a4d81b29aae2edb1f1c3fd085df6f3411
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-11 18:10:43 +0100

    [day11] move seat occupancy check to check routines

--- 11.py
+++ 11.py
@@ -8,6 +8,8 @@ def neighbours(position):
         and 0 <= check_row < total_rows and 0 <= check_seat < total_seats]
 
 def is_seat_desirable(input_configuration, position):
+    if input_configuration[position] != 'L':
+        return False
     count = 0
     checked_seats = 0
     for check_position in neighbours(position):
@@ -17,6 +19,8 @@ def is_seat_desirable(input_configuration, position):
     return checked_seats == count
 
 def is_seat_undesirable(input_configuration, position):
+    if input_configuration[position] != '#':
+        return False
     count = 0
     for check_position in neighbours(position):
         if check_position in input_configuration:
@@ -29,10 +33,10 @@ def update_configuration(input_configuration):
     output_configuration = input_configuration.copy()
     for position, occupancy in input_configuration.items():
         # take seat if desireable
-        if occupancy == 'L' and is_seat_desirable(input_configuration, position):
+        if is_seat_desirable(input_configuration, position):
             output_configuration[position] = '#'
         # leave seat if undesirable
-        if occupancy == '#' and is_seat_undesirable(input_configuration,position):
+        if is_seat_undesirable(input_configuration, position):
             output_configuration[position] = 'L'
     return output_configuration
 
