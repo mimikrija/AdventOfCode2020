c8ab841a60e42065775502b286cd9710d6a24c23
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-11 17:14:38 +0100

    [day11] use dictionary instead nested lists

--- 11.py
+++ 11.py
@@ -1,38 +1,39 @@
 import copy
 
-def neighbours(row_pos, seat_pos):
+def neighbours(position):
+    row_pos, seat_pos = position
     return [(check_row, check_seat)
         for check_row in range (row_pos-1, row_pos + 2) for check_seat in range (seat_pos-1, seat_pos+2)
         if (check_row != row_pos or check_seat != seat_pos)
         and 0 <= check_row < total_rows and 0 <= check_seat < total_seats]
 
-def is_seat_desirable(input_configuration,row_pos,seat_pos):
+def is_seat_desirable(input_configuration, position):
     count = 0
     checked_seats = 0
-    for check_x, check_y in neighbours(row_pos, seat_pos):
-        checked_seats += input_configuration[check_x][check_y] != '.'
-        count += input_configuration[check_x][check_y] == 'L'
+    for position in neighbours(position):
+        if position in input_configuration:
+            checked_seats += 1
+            count += input_configuration[position] == 'L'
     return checked_seats == count
 
-def is_seat_undesirable(input_configuration,row_pos,seat_pos):
+def is_seat_undesirable(input_configuration, position):
     count = 0
-    checked_seats = 0
-    for check_x, check_y in neighbours(row_pos, seat_pos):
-        count += input_configuration[check_x][check_y] == '#'
+    for position in neighbours(position):
+        if position in input_configuration:
+            count += input_configuration[position] == '#'
         if count >= 4:
             return True
     return False
 
 def update_configuration(input_configuration):
-    output_configuration = copy.deepcopy(input_configuration)
-    for row_pos in range(0, total_rows):
-        for seat_pos in range(0, total_seats):
-            # take seat if desireable
-            if input_configuration[row_pos][seat_pos] == 'L' and is_seat_desirable(input_configuration,row_pos,seat_pos):
-                output_configuration[row_pos][seat_pos] = '#'
-            # leave seat if undesirable
-            if input_configuration[row_pos][seat_pos] == '#' and is_seat_undesirable(input_configuration,row_pos,seat_pos):
-                output_configuration[row_pos][seat_pos] = 'L'
+    output_configuration = input_configuration.copy()
+    for position, occupancy in input_configuration.items():
+        # take seat if desireable
+        if occupancy == 'L' and is_seat_desirable(input_configuration, position):
+            output_configuration[position] = '#'
+        # leave seat if undesirable
+        if occupancy == '#' and is_seat_undesirable(input_configuration,position):
+            output_configuration[position] = 'L'
     return output_configuration
 
 # parse input
@@ -56,18 +57,21 @@ for row_c, row in enumerate(seat_configuration):
         if seat != '.':
             all_coordinates[(row_c,seat_c)] = seat
 
-next_configuration = []
+
+
+current_configuration = all_coordinates.copy()
+#output_configuration = {}
+
 while True:
-    next_configuration = update_configuration(seat_configuration)
-    if next_configuration == seat_configuration:
+    next_configuration = update_configuration(current_configuration)
+    if next_configuration == current_configuration:
         break
     else:
-        seat_configuration = copy.deepcopy(next_configuration)
+        current_configuration = next_configuration.copy()
 
 part_1 = 0
-for row in next_configuration:
-    for seat in row:
-        part_1 += seat == '#'
+for occupancy in current_configuration.values():
+    part_1 += occupancy == '#'
 
 print(f'The number of occupied seats is: {part_1}!')
 # The number of occupied seats is: 2204!
