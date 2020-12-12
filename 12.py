0286d0583a1dc9b58b33f873626b5de83ba57cbc
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-12 11:39:07 +0100

    [day12] first round of clean-up and comments

--- 12.py
+++ 12.py
@@ -1,11 +1,11 @@
 
 
 def get_direction(absolute_index):
-    sides = ['E','S','W','N']
+    SIDES = ['E','S','W','N']
     absolute_index = absolute_index % 4
-    return sides[absolute_index]
+    return SIDES[absolute_index]
 
-orientation_vector = {
+ORIENTATION_VECTOR = {
     'E': (1, 0),
     'S': (0, -1),
     'W': (-1, 0),
@@ -15,51 +15,53 @@ orientation_vector = {
 def manhattan_distance(coordinate):
     return sum(abs(p) for p in coordinate)
 
+# open and parse input
 with open('./inputs/12', 'r') as infile:
     instructions = infile.readlines()
 
+instructions = [(instruction[0], int(instruction[1:])) for instruction in instructions]
+
+# initial conditions part 1
 absolute_index = 0
 current_location = (0,0)
 
 # part 1
-for instruction in instructions:
-    command, amount = instruction[0], int(instruction[1:])
+for command, amount in instructions:
 
+    # rotate ship orientation
     if command == 'R':
         absolute_index += amount // 90
     if command == 'L':
         absolute_index -= amount // 90
-
     global_direction = get_direction(absolute_index)
 
+    # move the ship backward or forward (this is relative to orientation)
     if command == 'F':
-        current_location = tuple(current_location[i] + orientation_vector[global_direction][i] * amount for i in range(2))
+        current_location = tuple(current_location[i] + ORIENTATION_VECTOR[global_direction][i] * amount for i in range(2))
     if command == 'B':
-        current_location = tuple(current_location[i] - orientation_vector[global_direction][i] * amount for i in range(2))
+        current_location = tuple(current_location[i] - ORIENTATION_VECTOR[global_direction][i] * amount for i in range(2))
 
-    if command in orientation_vector:
-        current_location = tuple(current_location[i] + orientation_vector[command][i] * amount for i in range(2))
+    # move the ship in absolute (E, S, W, or N) direction
+    if command in ORIENTATION_VECTOR:
+        current_location = tuple(current_location[i] + ORIENTATION_VECTOR[command][i] * amount for i in range(2))
 
-print(manhattan_distance(current_location)) # 858
+print(f'Ship distance after following all the instructions is: {manhattan_distance(current_location)}')
+# Ship distance after following all the instructions is: 858
 
-# part 2 waypoints
 
+# part 2 inital conditions
 waypoint = (10, 1)
-ship = (0,0)
 current_location = (0,0)
-
-def get_distance(current, waypoint):
-    return tuple(waypoint[i] for i in range(2))
-
 
 ROTATION = {
     'L': 1j,
     'R': -1j
 }
 
-for instruction in instructions:
-    command, amount = instruction[0], int(instruction[1:])
+# part 2
+for command, amount in instructions:
 
+    # rotate the waypoint (relative to the ship)
     if command == 'R' or command == 'L':
         temp_x, temp_y = waypoint
         temp_waypoint= complex(temp_x, temp_y)
@@ -67,20 +69,17 @@ for instruction in instructions:
         for c in range (count):
             temp_waypoint *= ROTATION[command]
         
-        waypoint = (temp_waypoint.real, temp_waypoint.imag)
-  
+        waypoint = (int(temp_waypoint.real), int(temp_waypoint.imag))
 
     global_direction = get_direction(absolute_index)
-
-    if command in orientation_vector:
-        waypoint = tuple(waypoint[i] + orientation_vector[command][i] * amount for i in range(2))
-    print(waypoint)
+    
+    # move the waypoint (relative to the ship)
+    if command in ORIENTATION_VECTOR:
+        waypoint = tuple(waypoint[i] + ORIENTATION_VECTOR[command][i] * amount for i in range(2))
 
     # move the ship
     if command == 'F':
         current_location = tuple(current_location[i] + amount*waypoint[i] for i in range(2))
-        #waypoint = tuple(waypoint[i] + current_location[i] for i in range(2))
-
-    print(command, ' loc', current_location, 'relative waypoint:', waypoint)
  
-print(manhattan_distance(current_location)) # 39140
+print(f'Ship distance following new rules is: {manhattan_distance(current_location)}!') # 39140
+# Ship distance following new rules is: 39140!
