e522214308ddcad9e66cd19c59a5e3d8dd4c73d8
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-12 12:51:02 +0100

    [day12] refactor using complex numbers for everything

--- 12.py
+++ 12.py
@@ -1,19 +1,22 @@
+UNIT_VECTOR = {
+    'E': 1 + 0j,
+    'S': 0 - 1j,
+    'W': -1 + 0j,
+    'N': 0 + 1j
+}
 
+DIRECTION_FACTORS = {
+    'F': 1,
+    'B': -1
+}
 
-def get_direction(absolute_index):
-    SIDES = ['E','S','W','N']
-    absolute_index = absolute_index % 4
-    return SIDES[absolute_index]
-
-ORIENTATION_VECTOR = {
-    'E': (1, 0),
-    'S': (0, -1),
-    'W': (-1, 0),
-    'N': (0, 1)
+ROTATION = {
+    'L': 1j,
+    'R': -1j
 }
 
 def manhattan_distance(coordinate):
-    return sum(abs(p) for p in coordinate)
+    return int(sum(abs(p) for p in [coordinate.real, coordinate.imag]))
 
 # open and parse input
 with open('./inputs/12', 'r') as infile:
@@ -22,64 +25,50 @@ with open('./inputs/12', 'r') as infile:
 instructions = [(instruction[0], int(instruction[1:])) for instruction in instructions]
 
 # initial conditions part 1
-absolute_index = 0
-current_location = (0,0)
+
+current_location = 0 + 0j
+ship_direction = UNIT_VECTOR['E']
 
 # part 1
 for command, amount in instructions:
 
-    # rotate ship orientation
-    if command == 'R':
-        absolute_index += amount // 90
-    if command == 'L':
-        absolute_index -= amount // 90
-    global_direction = get_direction(absolute_index)
+    # rotate ship in increments of 90 degrees (L, R)
+    if command in ROTATION.keys():
+        for _ in range (amount//90):
+            ship_direction *= ROTATION[command]
 
-    # move the ship backward or forward (this is relative to orientation)
-    if command == 'F':
-        current_location = tuple(current_location[i] + ORIENTATION_VECTOR[global_direction][i] * amount for i in range(2))
-    if command == 'B':
-        current_location = tuple(current_location[i] - ORIENTATION_VECTOR[global_direction][i] * amount for i in range(2))
+    # move the ship backward or forward (B, F) in the ship_direction by amount
+    if command in DIRECTION_FACTORS.keys():
+        current_location += DIRECTION_FACTORS[command] * ship_direction * amount
 
-    # move the ship in absolute (E, S, W, or N) direction
-    if command in ORIENTATION_VECTOR:
-        current_location = tuple(current_location[i] + ORIENTATION_VECTOR[command][i] * amount for i in range(2))
+    # move the ship in absolute (E, S, W, or N) direction by amount
+    if command in UNIT_VECTOR:
+        current_location += UNIT_VECTOR[command] * amount
 
-print(f'Ship distance after following all the instructions is: {manhattan_distance(current_location)}')
-# Ship distance after following all the instructions is: 858
+print(f'Ship distance after following all the instructions is: {manhattan_distance(current_location)}!')
+# Ship distance after following all the instructions is: 858!
 
 
 # part 2 inital conditions
-waypoint = (10, 1)
-current_location = (0,0)
-
-ROTATION = {
-    'L': 1j,
-    'R': -1j
-}
+waypoint = 10 + 1j
+current_location = 0 + 0j
 
 # part 2
 for command, amount in instructions:
+    # waypoint is always relative to the ship location!
 
-    # rotate the waypoint (relative to the ship)
-    if command == 'R' or command == 'L':
-        temp_x, temp_y = waypoint
-        temp_waypoint= complex(temp_x, temp_y)
-        count = amount // 90
-        for c in range (count):
-            temp_waypoint *= ROTATION[command]
-        
-        waypoint = (int(temp_waypoint.real), int(temp_waypoint.imag))
+    # rotate waypoint in increments of 90 degrees (L, R)
+    if command in ROTATION.keys():
+        for _ in range (amount//90):
+            waypoint *= ROTATION[command]
 
-    global_direction = get_direction(absolute_index)
-    
-    # move the waypoint (relative to the ship)
-    if command in ORIENTATION_VECTOR:
-        waypoint = tuple(waypoint[i] + ORIENTATION_VECTOR[command][i] * amount for i in range(2))
+    # move the waypoint in absolute (E, S, W, or N) direction by amount
+    if command in UNIT_VECTOR:
+        waypoint += UNIT_VECTOR[command] * amount
 
-    # move the ship
+    # move the in the waypoint direction by amount
     if command == 'F':
-        current_location = tuple(current_location[i] + amount*waypoint[i] for i in range(2))
+        current_location += waypoint * amount
  
 print(f'Ship distance following new rules is: {manhattan_distance(current_location)}!') # 39140
 # Ship distance following new rules is: 39140!
