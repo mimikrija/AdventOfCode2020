3ef23db04b672e256136dbb8e5e688f21bb758be
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-12 20:44:34 +0100

    [day12] put actions into functions

--- 12.py
+++ 12.py
@@ -23,25 +23,32 @@ instructions = [(instruction[0], int(instruction[1:])) for instruction in instru
 
 current_location = 0 + 0j
 ship_direction = UNIT_VECTOR['E']
-def move_it(command, by_amount):
+def move_it(command, amount):
     # move the ship in absolute (E, S, W, or N) direction by amount
-    if command in UNIT_VECTOR:
-        return UNIT_VECTOR[command] * by_amount
-    return 0 + 0j
+    return UNIT_VECTOR[command] * amount
+
+def rotate_it(command, amount):
+    # rotate in increments of 90 degrees (L, R)
+    direction = 1 + 0j
+    for _ in range (amount//90):
+        direction *= ROTATION[command]
+    return direction
+
+def go_forward(direction, amount):
+    return direction * amount
+
 # part 1
 for command, amount in instructions:
-
-    # rotate ship in increments of 90 degrees (L, R)
     if command in ROTATION.keys():
-        for _ in range (amount//90):
-            ship_direction *= ROTATION[command]
+        ship_direction *= rotate_it(command, amount)
+
+    # move the ship in absolute (E, S, W, or N) direction by amount
+    if command in UNIT_VECTOR:
+        current_location += move_it(command, amount)
 
     # move the ship backward or forward (F) in the ship_direction by amount
     if command == 'F':
-        current_location += ship_direction * amount
-
-    # move the ship in absolute (E, S, W, or N) direction by amount
-    current_location += move_it(command, amount)
+        current_location += go_forward(ship_direction, amount)
 
 print(f'Ship distance after following all the instructions is: {manhattan_distance(current_location)}!')
 # Ship distance after following all the instructions is: 858!
@@ -55,17 +62,17 @@ current_location = 0 + 0j
 for command, amount in instructions:
     # waypoint is always relative to the ship location!
 
-    # rotate waypoint in increments of 90 degrees (L, R)
+    # rotate waypoint in increments of 90 degrees (L,R)
     if command in ROTATION.keys():
-        for _ in range (amount//90):
-            waypoint *= ROTATION[command]
+        waypoint *= rotate_it(command, amount)
 
     # move the waypoint in absolute (E, S, W, or N) direction by amount
-    waypoint += move_it(command, amount)
+    if command in UNIT_VECTOR:
+        waypoint += move_it(command, amount)
 
     # move the in the waypoint direction by amount
     if command == 'F':
-        current_location += waypoint * amount
+        current_location += go_forward(waypoint, amount)
  
 print(f'Ship distance following new rules is: {manhattan_distance(current_location)}!') # 39140
 # Ship distance following new rules is: 39140!
