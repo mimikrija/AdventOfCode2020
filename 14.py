2b7c11620306a0f331a0c90d60e658482151763a
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-14 07:24:29 +0100

    [day14] first go at part 1, parsing was again a drag

---
+++ 14.py
@@ -0,0 +1,38 @@
+def to_binary(num):
+    num = int(num)
+    num_bin = "{0:036b}".format(num)
+    return num_bin
+
+
+def apply_bitmap(memory_dict, mask, memory_location, memory_value):
+    memory_bin =  to_binary(memory_value)
+    for n, c in enumerate(mask):
+        if c != 'X':
+            memory_bin = memory_bin[:n] + c + memory_bin[n+1:]
+    memory_dict[memory_location] = memory_bin
+
+
+
+# read data in bulks split by \n\n :)
+inputs = open('inputs/14').read().split('mask = ')
+inputs = inputs[1:]
+
+program_memory = {}
+
+
+
+for chunk in inputs:
+    chunk = chunk.split('\n')
+    for n, line in enumerate(chunk):
+        if line == '':
+            continue
+        if n == 0:
+            mask = line
+        else:
+            memory, to_write = line.split(' = ')
+            apply_bitmap(program_memory, mask, memory, to_write)
+
+part_1 = sum( [int(memory,2) for memory in program_memory.values()])
+
+
+print(part_1) # 13727901897109
