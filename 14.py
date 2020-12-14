6b9bf0b492f956a4fe9cbec872c21bc6dc98bdfa
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-14 10:48:16 +0100

    [day14] add functions for address decoding

--- 14.py
+++ 14.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from itertools import combinations
+
 def to_binary(num):
     num_bin = "{0:036b}".format(num)
     return num_bin
@@ -8,6 +11,25 @@ def apply_bitmask(memory_dict, mask, memory_location, memory_value):
         if c != 'X':
             memory_bin = memory_bin[:n] + c + memory_bin[n+1:]
     memory_dict[memory_location] = memory_bin
+
+def get_floating(mask, memory_ad):
+    """ generates a memory address with X's """
+    memory_with_X = to_binary(memory_ad)
+    for n, c in enumerate(mask):
+        if c != '0':
+            memory_with_X = memory_with_X[:n] + c + memory_with_X[n+1:]
+    return(memory_with_X)
+
+def generate_adresses(base_address, memory_floating):
+    """returns a list of address keys where a value should be written"""
+    addresses_additions = [0]
+    all_additions = [ 2**(35-n) for n, c in enumerate(memory_floating) if c == 'X' ]
+    # generate all combinations of values to add
+    for n in range (1, len(all_additions)+1):
+        addresses_additions += [sum([num for num in combo]) for combo in combinations(all_additions,n)]
+    # generate a list of keys for memory values
+    address_list = [ 'mem[' + str(base_address + add) + ']' for add in addresses_additions]
+    return(address_list)
 
 
 
