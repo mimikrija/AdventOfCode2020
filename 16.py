eda80d82a44adc888721bff069e6a1851986c4a8
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-16 07:13:08 +0100

    [day15] return all invalid values from a ticket

--- 16.py
+++ 16.py
@@ -1,4 +1,16 @@
-def is_value_valid(rule, ticket_field):
-    l_1, u_1, l_2, u_2 = rule
-    return ticket_field in range(l_1, u_1 + 1) or ticket_field in range(l_2, u_2 + 1)
+def invalid_value(rules, ticket_field):
+    """if present, returns invalid ticket field"""
+    is_invalid = True
+    for rule in rules:
+        l_1, u_1, l_2, u_2 = rule
+        is_invalid &= ticket_field not in range(l_1, u_1 + 1) and ticket_field not in range(l_2, u_2 + 1)
+    if is_invalid:
+        return ticket_field
+    else:
+        return 0
 
+# part_1 = 0
+# for ticket_value in ticket:
+#         part_1 += invalid_value(rules, ticket_value)
+
+# print(part_1)
