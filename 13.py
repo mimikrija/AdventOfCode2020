c0029c996b112eb9142befddfbf7c704bcabc1bb
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-18 19:04:45 +0100

    [day13] part 2 "first" go, is this CRT?
    anyway, after a lot of papers and equations and hints pointing towards
    CRM (which I implemented but worked only for some test cases, wtf?), I
    found [this](https://www.reddit.com/r/adventofcode/comments/kcb3bb/2020_day_13_part_2_can_anyone_tell_my_why_this/)
    thread on Reddit, which was the approach I was using in my notes, but I
    think I was missing the fact that we need to increment by the `period`,
    ie LCM of the busses so far.

--- 13.py
+++ 13.py
@@ -1,7 +1,7 @@
 import re
 import math
 
-with open('inputs/13-ex') as inputfile:
+with open('inputs/13') as inputfile:
     inputs = inputfile.readlines()
 
 digits = re.compile(r'[0-9]+')
@@ -17,61 +17,23 @@ minutes_to_wait = my_time-desired_time
 
 print(f'Part 1 solution is: {minutes_to_wait*all_times[my_time]}!') # 333
 
-# part 2
-
-# def sum_until():
-#     time = 1068779
-#     is_it = ((time+inc)%bus == 0  for inc, bus in zip(time_diffs, bus_IDs))
-#     print(all(is_it))
-#     yield all(is_it), time
-#     time += 1
-
 input_line_two = inputs[1].strip().split(',')
-
 time_diffs = [ n for n, c in enumerate(input_line_two) if c != 'x']
 
-print(time_diffs)
-print(bus_IDs)
-
-def first_overlap_and_period(bus_1, diff_1, bus_2, diff_2):
-    for time in range (bus_1, bus_1*bus_2+1):
+def first_overlap(time_start, bus_1, diff_1, bus_2, diff_2, delta):
+    for time in range (time_start, bus_1*bus_2*delta+1, delta):
         if (time + diff_1) % bus_1 == 0 and (time + diff_2) % bus_2 == 0:
-            return (time, bus_1*bus_2)
+            return time
 
-print(first_overlap_and_period(7, 13, 0, 1))
+time = 0
+period = bus_IDs[0]
+for n in range(len(bus_IDs)-1):
+    b_1 = bus_IDs[n]
+    b_2 = bus_IDs[n+1]
+    d_1 = time_diffs[n]
+    d_2 = time_diffs[n+1]
+    time = first_overlap(time, b_1, d_1, b_2,d_2, period)
+    period *= b_2
 
-diff = time_diffs[0]
-num = bus_IDs[0]
-for n in range(1,len(bus_IDs)):
-    print(num, diff, bus_IDs[n], time_diffs[n])
-    num, diff = first_overlap_and_period(num, diff, bus_IDs[n], time_diffs[n])
-    print(num, diff)
-    
-
-
-
-quit()
-        
-
-
-
-def chinese_remainder(numbers, remainders):
-    result = 0
-    product = math.prod(numbers)
-    prod_div_by_reminders = [product // remainder for remainder in remainders]
-    mod_mult_inverse = [(prod_div_by_reminders[n] * remainders[n])%numbers[n]
-            for n in range(len(numbers))]
-    for n in range(len(numbers)):
-        result+= (remainders[n]*prod_div_by_reminders[n]*mod_mult_inverse[n])
-    return result%product
-
-part2 = chinese_remainder(bus_IDs[1:],time_diffs[1:])
-print(part2) # 30309578578160 not correct
-print(part2%29)
-for n,r in list(zip(bus_IDs,time_diffs)):
-    if n != 0:
-        print(n, part2%int(n),r)
-print((part2*9)%443)
-
-
-# 181857471544305 not correct
+print(f'First time they align with offsets {time_diffs} is {time}!')
+# First time they align with offsets [0, 23, 29, 37, 42, 46, 48, 60, 101] is 690123192779524!
