6909e07959ec8585a925ec258e7176faa7989b7d
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-18 11:33:12 +0100

    [day18] this works for a single () pair

--- 18.py
+++ 18.py
@@ -13,4 +13,18 @@ def calculate_list(in_list):
             operation += c
         result = eval(operation)
         in_list = [str(result)] + in_list[3:]
-    return result
+    return str(result)
+
+def get_rid_of_brackets(in_list):
+    if '(' not in in_list:
+        return([calculate_list(in_list)])
+    else:
+        pos_1 = in_list.index('(')
+        pos_2 = in_list.index(')')
+        in_list = in_list[:pos_1] + get_rid_of_brackets(in_list[pos_1+1:pos_2]) + in_list[pos_2+1:]
+
+    return (calculate_list(in_list))
+
+for operation in operations:
+    print(operation)
+    print(get_rid_of_brackets(operation))
