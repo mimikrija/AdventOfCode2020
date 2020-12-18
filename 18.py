78149926c9e5c2ae4f7a54ccb8e618ecad73e082
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-18 10:59:33 +0100

    [day18] add basic calculation (no "()") function

--- 18.py
+++ 18.py
@@ -5,3 +5,12 @@ with open('inputs/18-ex') as inputfile:
 
 re_math = re.compile(r'[0-9+*()]')
 operations = [ re.findall(re_math, line) for line in inputs]
+
+def calculate_list(in_list):
+    while len(in_list) >= 3:
+        operation = ''
+        for c in in_list[:3]:
+            operation += c
+        result = eval(operation)
+        in_list = [str(result)] + in_list[3:]
+    return result
