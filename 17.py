58bc7fb642bfd901b8c81bef462e816aaa0571db
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-18 20:59:14 +0100

    [day17][temp] use sets instead of dict
    activation and deactivation can all be done in one go. when we loop
    through the neighbors we can generate a list of neighbors for each cube,
    count the neighbors of that cube and activate them if needed

--- 17.py
+++ 17.py
@@ -1,146 +1,39 @@
 from collections import Counter
+import itertools
+dimension = 3
+deltas = set(itertools.product({-1,0,1}, repeat=dimension))
 
-def count_neighbors(cube, positions):
-    x, y, z = cube
-    counter = 0
-    for delta_x in {-1, 0, 1}:
-         for delta_y in {-1, 0, 1}:
-              for delta_z in {-1, 0, 1}:
-                    if delta_x == delta_y == delta_z == 0:
-                        continue
-                    else:
-                        counter += positions.get((x+delta_x, y+delta_y, z+delta_z), '.') == '#'
-    return counter
-
-def generate_neighbors(cube, in_positions):
-    known_positions = set(in_positions.keys())
-    x, y, z = cube
-    new_neighbors = set()
-    for delta_x in {-1, 0, 1}:
-         for delta_y in {-1, 0, 1}:
-              for delta_z in {-1, 0, 1}:
-                    new_cube = (x+delta_x, y+delta_y, z+delta_z)
-                    if delta_x == delta_y == delta_z == 0:
-                        continue
-                    else:
-                        new_neighbors.add(new_cube)
-    return new_neighbors - known_positions
-
-def widen_the_map(in_positions):
-    positions = in_positions.copy()
-    all_neighbors = set()
-    for cube in in_positions:
-        all_neighbors = all_neighbors.union(generate_neighbors(cube, in_positions))
-    for new_neighbor in all_neighbors:
-        positions[new_neighbor] = '.'
-    return positions
-
-def de_acitivation(in_positions):
-    positions = in_positions.copy()
-    for cube in in_positions:
-        # active becomes inactive
-        if in_positions.get(cube, '.') == '#' and count_neighbors(cube, in_positions) not in {2, 3}:
-            positions[cube] = '.'
-        # inactive becomes active
-        else:
-            if in_positions.get(cube, '.') == '.' and count_neighbors(cube, in_positions) == 3:
-                positions[cube] = '#'
-    return positions
-
-def purge_the_map(in_positions):
-    out_positions= {}
-    for cube, occupancy in in_positions.items():
-        if occupancy != '.':
-            out_positions[cube] = '#'
-    return out_positions
-
-with open('inputs/17') as inputfile:
+with open('inputs/17-ex') as inputfile:
     inputs = inputfile.readlines()
-
-# parse input into a dictionary of cube coordinates (z is initialized to 0)
-initial_configuration = {(row_num, column_num, 0): column
+# parse input into a set
+active_cubes = set((row_num, column_num, 0)
                         for row_num, row in enumerate(inputs)
                         for column_num, column in enumerate(row.strip())
-                        if column != '.'}
+                        if column == '#')
+dim_x = len(inputs[0].strip())
+dim_y = len(inputs)
+flat_dim = max(dim_x, dim_y)
 
-previous_configuration = initial_configuration.copy()
-for n in range (6):
-    current_configuration = widen_the_map(previous_configuration)
-    current_configuration = de_acitivation(current_configuration)
-    current_configuration = purge_the_map(current_configuration)
-    previous_configuration = current_configuration.copy()
+# domain = itertools.product(range(-flat_dim*6,flat_dim*6+1), repeat = dimension)
+# domain_purged_3d = (di for di in domain if di[2] in range(-6, 7))
 
-part_1 = Counter(current_configuration.values())['#']
-print(f'Number of active cells after {n+1} cycles is: {part_1}!')
-# Number of active cells after 6 cycles is: 346!
-
+def add_coordinates(tuple_1, tuple_2):
+    return tuple(t_1 + t_2 for t_1, t_2 in zip(tuple_1,tuple_2))
 
 def count_neighbors(cube, positions):
-    x, y, z, w  = cube
+    out_positions = positions.copy()
     counter = 0
-    for delta_x in {-1, 0, 1}:
-         for delta_y in {-1, 0, 1}:
-              for delta_z in {-1, 0, 1}:
-                  for delta_w in {-1, 0, 1}:
-                        if delta_x == delta_y == delta_z == delta_w == 0:
-                            continue
-                        else:
-                            counter += positions.get((x+delta_x, y+delta_y, z+delta_z, w+delta_w), '.') == '#'
-    return counter
+    neighbors = []
+    for delta in deltas:
+        if delta == (0, 0, 0):
+            continue
+        neighbor = add_coordinates(cube, delta)
+        neighbors.append(neighbor)
+        counter += neighbor in positions
+    if counter not in {2, 3}:
+        positions.remove(cube)
+    # check neighbors, add  if it needs to be activated
+    return #return out_positions counter
 
-def generate_neighbors(cube, in_positions):
-    known_positions = set(in_positions.keys())
-    x, y, z, w = cube
-    new_neighbors = set()
-    for delta_x in {-1, 0, 1}:
-         for delta_y in {-1, 0, 1}:
-              for delta_z in {-1, 0, 1}:
-                   for delta_w in {-1, 0, 1}:
-                        new_cube = (x+delta_x, y+delta_y, z+delta_z, w+delta_w)
-                        if delta_x == delta_y == delta_z == delta_w == 0:
-                                continue
-                        else:
-                            new_neighbors.add(new_cube)
-    return new_neighbors - known_positions
+#print(count_neighbors((1,2,0),active_cubes))
 
-def widen_the_map(in_positions):
-    positions = in_positions.copy()
-    all_neighbors = set()
-    for cube in in_positions:
-        all_neighbors = all_neighbors.union(generate_neighbors(cube, in_positions))
-    for new_neighbor in all_neighbors:
-        positions[new_neighbor] = '.'
-    return positions
-
-def de_acitivation(in_positions):
-    positions = in_positions.copy()
-    for cube in in_positions:
-        # active becomes inactive
-        if in_positions.get(cube, '.') == '#' and count_neighbors(cube, in_positions) not in {2, 3}:
-            positions[cube] = '.'
-        # inactive becomes active
-        else:
-            if in_positions.get(cube, '.') == '.' and count_neighbors(cube, in_positions) == 3:
-                positions[cube] = '#'
-    return positions
-
-with open('inputs/17') as inputfile:
-    inputs = inputfile.readlines()
-
-# parse input into a dictionary of cube coordinates (z is initialized to 0)
-initial_configuration = {(row_num, column_num, 0, 0): column
-                        for row_num, row in enumerate(inputs)
-                        for column_num, column in enumerate(row.strip())
-                        if column != '.'}
-
-previous_configuration = initial_configuration.copy()
-for n in range (6):
-    current_configuration = widen_the_map(previous_configuration)
-    current_configuration = de_acitivation(current_configuration)
-    current_configuration = purge_the_map(current_configuration)
-    previous_configuration = current_configuration.copy()
-    part_2 = Counter(current_configuration.values())['#']
-
-print(f'Number of active cells after {n+1} cycles is: {part_2}!')
-# Number of active cells after 6 cycles is: 1632!
-# Time needed = 7.761 s
