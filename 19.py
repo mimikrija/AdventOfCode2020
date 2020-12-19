66421bd5c0caaf88bc5f07f3d5362c9522d14159
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-19 09:17:09 +0100

    [day19] return matches for simple example
    in order to generalise this I had to save the original applied rules
    values as a list, but this way it is more consistent. of course it
    could make sense to use sets instead of lists?

--- 19.py
+++ 19.py
@@ -3,7 +3,7 @@
 # 1: "a"
 # 2: 1 3 | 3 1
 # 3: "b"
-applied_rules = {3: "b", 1: "a"} # I think I can use eval to do this directly on input if "letter in input"/"numbers not present in input"/"only one space present in input"
+applied_rules = {3: ["b"], 1: ["a"]} # I think I can use eval to do this directly on input if "letter in input"/"numbers not present in input"/"only one space present in input"
 not_applied_rules = {0: [(1,2)], 2: [(1,3),(3,1)]}
 
 def who_can_I_apply_next(applied_rules, not_applied_rules):
@@ -12,4 +12,17 @@ def who_can_I_apply_next(applied_rules, not_applied_rules):
         if all(rule in applied_rules.keys() for rule in to_be_applied):
             return rule_no
 
-print(who_can_I_apply_next(applied_rules,not_applied_rules))
+def apply_rule(applied_rules, not_applied_rules, rule_no):
+    rule_pairs = not_applied_rules[rule_no]
+    result = []
+    for first, second in rule_pairs:
+        result += [ one + two for one in applied_rules[first] for two in applied_rules[second]]
+    return result
+
+while not_applied_rules:
+    next_rule = who_can_I_apply_next(applied_rules, not_applied_rules)
+    applied_rules[next_rule] = apply_rule(applied_rules, not_applied_rules, next_rule)
+    del not_applied_rules[next_rule]
+
+matches = applied_rules[0]
+print(matches)
