b859aff9a354fb14606068bc7e3c4a26b4afaa4c
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-19 10:35:43 +0100

    [day19] expand functions to work with example 2:
    for example, the tuple of rules is not necessarily always of size 2. I
    scaned through my input briefly and it does seem that sizes 1, 2, and 3
    should cover all cases.
    
    furhermore, it is possible that two rules simutlaneouosly could be
    applied next so I expanded `who to apply next` to return a list of rules
    to all be applied in the next go.
    
    next step: parsing the input! (ugh)

--- 19.py
+++ 19.py
@@ -3,27 +3,44 @@
 # 1: "a"
 # 2: 1 3 | 3 1
 # 3: "b"
-applied_rules = {3: ["b"], 1: ["a"]} # I think I can use eval to do this directly on input if "letter in input"/"numbers not present in input"/"only one space present in input"
-not_applied_rules = {0: [(1,2)], 2: [(1,3),(3,1)]}
+applied_rules = {4: ["a"], 5: ["b"]} # I think I can use eval to do this directly on input if "letter in input"/"numbers not present in input"/"only one space present in input"
+not_applied_rules = {0: [(4, 1, 5)], 1: [(2,3),(3,2)], 2: [(4,4), (5, 5)], 3: [(4, 5), (5, 4)]}
 
 def who_can_I_apply_next(applied_rules, not_applied_rules):
+    rules_to_be_applied_next = []
     for rule_no, rule in not_applied_rules.items():
         to_be_applied = set(n for tup in rule for n in tup)
         if all(rule in applied_rules.keys() for rule in to_be_applied):
-            return rule_no
+            rules_to_be_applied_next.append(rule_no)
+    return rules_to_be_applied_next
 
 def apply_rule(applied_rules, not_applied_rules, rule_no):
-    rule_pairs = not_applied_rules[rule_no]
+    rule_tuples = not_applied_rules[rule_no]
     result = set()
-    for first, second in rule_pairs:
-        test = {one + two for one in applied_rules[first] for two in applied_rules[second]}
+    for rule_tuple in rule_tuples:
+        if len(rule_tuple) == 1:
+            first = rule_tuple[0]
+            test = {one for one in applied_rules[first]}
+        if len(rule_tuple) == 2:
+            first, second = rule_tuple
+            test = {one + two for one in applied_rules[first] for two in applied_rules[second]}
+        if len(rule_tuple) == 3:
+            first, second, third = rule_tuple
+            test = {one + two + three for one in applied_rules[first] for two in applied_rules[second] for three in applied_rules[third]}
         result = result.union(test)
     return set(result)
 
 while not_applied_rules:
-    next_rule = who_can_I_apply_next(applied_rules, not_applied_rules)
-    applied_rules[next_rule] = apply_rule(applied_rules, not_applied_rules, next_rule)
-    del not_applied_rules[next_rule]
+    next_rules = who_can_I_apply_next(applied_rules, not_applied_rules)
+    for next_rule in next_rules:
+        applied_rules[next_rule] = apply_rule(applied_rules, not_applied_rules, next_rule)
+        del not_applied_rules[next_rule]
 
 matches = applied_rules[0]
 print(matches)
+
+
+
+# test if the example works
+if all(test in matches for test in ['aaaabb', 'aaabab', 'abbabb', 'abbbab', 'aabaab', 'aabbbb', 'abaaab', 'ababbb']):
+    print('succes!')
