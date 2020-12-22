cc55e735fca77e65c429e2f0bf2ce311325aca6e
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-22 16:01:28 +0100

    [day22] comment out print statements (game??)
    I guess I need to save `game` as some sort of stach as well but at this
    point I am to lazy to do so

--- 22.py
+++ 22.py
@@ -43,16 +43,15 @@ def play_the_game(deck_1, deck_2):
         if deck:
             return deck
 
-game = 0
-def play_recursive_combat(deck_1, deck_2, game):
-    round = 0
-    game += 1
+
+def play_recursive_combat(deck_1, deck_2):
+    # round = 0
     configurations_1 = []
     configurations_2 = []
-    # print(f'=== Game {game} ===')
+    # print(f'\n=== Game {game} ===')
     while len(deck_1) > 0 and len(deck_2) > 0:
-        round += 1
-        # print(f'-- Round {round} (Game {game}) --')
+        # round += 1
+        # print(f'\n-- Round {round} (Game {game}) --')
         # print(f"Player 1's deck: {deck_1}")
         # print(f"Player 2's deck: {deck_2}")
         # check if the decks are in one of the previous configurations
@@ -74,9 +73,10 @@ def play_recursive_combat(deck_1, deck_2, game):
 
             # If both players have at least as many cards remaining in their deck
             # as the value of the card they just drew...
-            if len(deck_1) >= card_1 and len(deck_2) >= card_2:
+            if all(len(deck) >= card for deck, card in zip((deck_1, deck_2), (card_1, card_2))):
                 # the winner of the round is determined by playing a new game of Recursive Combat
                 # which is played by the remaining cards in their deck
+                # print('Playing a sub-game to determine the winner...\n')
                 new_deck_1 = deque()
                 for n, card in enumerate(deck_1):
                     if card_1 > n:
@@ -85,28 +85,32 @@ def play_recursive_combat(deck_1, deck_2, game):
                 for n, card in enumerate(deck_2):
                     if card_2 > n:
                         new_deck_2.append(card)
-                winner = play_recursive_combat(new_deck_1, new_deck_2, game)
+                winner = play_recursive_combat(new_deck_1, new_deck_2)
+                # print(f'...anyway, back to game {parent_game}.')
+
                 if winner == new_deck_1:
                     deck_1.append(card_1)
                     deck_1.append(card_2)
+                    # player = 'Player 1'
                 else:
                     deck_2.append(card_2)
                     deck_2.append(card_1)
+                    # player = 'Player 2'
             else:
                 # otherwise, the winner of the round is the player with the higher-value card
                 if card_1 > card_2:
                     deck_1.append(card_1)
                     deck_1.append(card_2)
                     winner = deck_1
-                    player = 'Player 1'
+                    # player = 'Player 1'
                 else:
                     deck_2.append(card_2)
                     deck_2.append(card_1)
                     winner = deck_2
-                    player = 'Player 2'
-        # print(f"{player} wins round {round} of game {game}!\n")
+                    # player = 'Player 2'
+    #     print(f"{player} wins round {round} of game {game}!")
+    # print(f'The winner of game {game} is {player}\n')
     return winner
-
 
 
 player_me, player_crab = both_decks
@@ -115,6 +119,6 @@ print(f'The winning score after one game is {part_1}!')
 # The winning score after one game is 32199!
 
 player_me, player_crab = both_decks
-part_2 = get_score(play_recursive_combat(player_me.copy(), player_crab.copy(), 0))
+part_2 = get_score(play_recursive_combat(player_me.copy(), player_crab.copy()))
 print(f'The winning score after >> RECURSIVE COMBAT CRAB vs ME << is {part_2}!')
 # The winning score after >> RECURSIVE COMBAT CRAB vs ME << is 33780!
