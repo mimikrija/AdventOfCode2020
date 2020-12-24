bc213454d5f7a64750481f71b31c3d9d9a45bc65
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-24 12:19:25 +0100

    [day24] party 2: if you expand the map USE IT!!
    I was expanding the map, but looping through `in_floor` which was
    causing some of the white tiles no to be checked....

--- 24.py
+++ 24.py
@@ -16,6 +16,43 @@ re_directions = re.compile(r'nw|ne|se|sw|e|w')
 def hex_add(in_hex_a, in_hex_b):
     return tuple(coordinate_a + coordinate_b for coordinate_a, coordinate_b in zip(in_hex_a, in_hex_b))
 
+def hex_neighbor(in_hex, direction):
+    return hex_add(in_hex, HEX_DIRECTIONS[direction])
+
+def tile_neighbors(in_hex):
+    return [hex_neighbor(in_hex, direction) for direction in HEX_DIRECTIONS]
+
+def art_installation(in_floor):
+    new_floor = in_floor.copy()
+    expanded_floor = new_floor.copy()
+    for tile in in_floor:
+        # expand mesh
+        neighbors = tile_neighbors(tile)
+        for neighbor in neighbors:
+            if neighbor in expanded_floor:
+                continue
+            else:
+                expanded_floor[neighbor] = False
+
+    for tile, black in expanded_floor.items():
+        neighbors = tile_neighbors(tile)
+        count_black_adjacents = sum(expanded_floor.get(candidate, False) for candidate in neighbors)
+        
+        # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+        if black:
+            if count_black_adjacents == 0 or count_black_adjacents > 2:
+                new_floor[tile] = False
+        # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+        else:
+            if count_black_adjacents == 2:
+                new_floor[tile] = True
+
+
+    return new_floor
+
+def count_black_tiles(in_floor):
+    return sum(black for black in in_floor.values())
+
 instructions = [re.findall(re_directions, line) for line in inputs]
 
 flipped_tiles = {}
@@ -28,6 +65,14 @@ for instruction in instructions:
     else:
         flipped_tiles[position] = True
 
-part_1 = sum(value for value in flipped_tiles.values())
+part_1 = count_black_tiles(flipped_tiles)
 print(f'After all the flipping, {part_1} tiles are left black side up!')
 # After all the flipping, 469 tiles are left black side up!
+
+current_floor = flipped_tiles.copy()
+
+
+for day in range(1, 101):
+    current_floor = art_installation(current_floor)
+
+print(count_black_tiles(current_floor)) # 4353
