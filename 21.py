5277ece372cb13f9f27a5fe8d09a53fc85e716c7
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-29 20:30:01 +0100

    [day21] simplify elimination

--- 21.py
+++ 21.py
@@ -1,5 +1,5 @@
 from collections import Counter
-with open('inputs/21-ex') as inputfile:
+with open('inputs/21') as inputfile:
     inputs = inputfile.readlines()
 
 # parse input into all_foods (list of tupples (alergens, ingredients))
@@ -20,40 +20,38 @@ for line in inputs:
 potential_alergens = {alergen: set() for alergen in ALL_ALERGENS}
 for alergens, ingredients in all_foods:
     for alergen in alergens:
-        # if alergen not in potential_alergens.keys():
-        #     potential_alergens[alergen] = ingredients
-        # else:
         potential_alergens[alergen] = potential_alergens[alergen].union(ingredients)
 
+determined_alergens = dict()
+for alergen in ALL_ALERGENS:
+    result = ALL_INGREDIENTS.copy()
+    for alergens, ingredients in all_foods:
+        if alergen in alergens:
+            result = result.intersection(ingredients)
+    potential_alergens[alergen] = result
 
+# keep eliminating as long as the number of determined alergens
+# is less than number of total alergens
+while len(determined_alergens) < len(ALL_ALERGENS):
+    for alergen, potential_ingredients in potential_alergens.items():
+        # eliminate already determined ingredients from the potential ingredients
+        candidates = potential_ingredients.difference(set(determined_alergens.values()))
+        if len(candidates) == 1:
+            determined_alergens[alergen] = candidates.pop()
 
-found_alergens = {}
-while potential_alergens:
-    for alergen in ALL_ALERGENS:
-        result = ALL_INGREDIENTS.copy()
-        for alergens, ingredients in all_foods:
-            if alergen in alergens:
-                result = result.intersection(ingredients)
-        if len(result) == 1:
-            solution = result.pop()
-            for key, value in potential_alergens.items():
-                if solution in value:
-                    potential_alergens[key].remove(solution)
-            found_alergens[alergen] = solution
-            del potential_alergens[alergen]
-
-
-not_alergens = ALL_INGREDIENTS.difference(found_alergens.values())
+not_alergens = ALL_INGREDIENTS.difference(set(determined_alergens.values()))
 part_1 = 0
 
 for _, ingredients in all_foods:
     for ingredient in ingredients:
         part_1 += ingredient in not_alergens
 
-part_2 = ','.join(item for item in sorted(found_alergens))
+
+part_2 = ','.join(item for item in sorted(determined_alergens))
 
 print(f'Non-alergens appear in my list of foods {part_1} times!')
 # Non-alergens appear in my list of foods 1977 times!
+
 
 print(f'The list of my dangerous ingredients is: {part_2}')
 # The list of my dangerous ingredients is: dairy,eggs,fish,nuts,peanuts,sesame,shellfish,wheat!
