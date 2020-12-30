b141d1ef5f1bfc9a77cfa5f59407ed1b51312f27
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-30 18:33:44 +0100

    [day24] simplify solution/concept using sets
    there is no need to store white tiles in the dictionary. it is enough to
    have a set of black tiles. the only white tiles that have potential to
    become black tiles, are black tiles' neighbours - hence we check in the
    expanded mesh. I think this can be further simplified but I will do it
    in stages.

--- 24.py
+++ 24.py
@@ -20,50 +20,47 @@ def hex_neighbor(in_hex, direction):
     return hex_add(in_hex, HEX_DIRECTIONS[direction])
 
 def tile_neighbors(in_hex):
-    return [hex_neighbor(in_hex, direction) for direction in HEX_DIRECTIONS]
+    return set(hex_neighbor(in_hex, direction) for direction in HEX_DIRECTIONS)
 
-def art_installation(in_floor):
-    new_floor = {}
-    expanded_floor = {tile: black for tile, black in in_floor.items() if black}
-    for tile in in_floor:
+def art_installation(in_black_tiles):
+    new_floor = in_black_tiles.copy()
+    expanded_floor = in_black_tiles.copy()
+    for tile in in_black_tiles:
         # expand mesh
-        neighbors = tile_neighbors(tile)
-        for neighbor in neighbors:
-            expanded_floor[neighbor] = in_floor.get(neighbor, False)
+        expanded_floor |= tile_neighbors(tile)
 
     # LOOP THROUGH EXPANDED MESH OTHERWISE THERE IS REALLY NO POINT OF EXPANDING IS IT?!
-    for tile, black in expanded_floor.items():
+    for tile in expanded_floor:
         neighbors = tile_neighbors(tile)
-        count_black_adjacents = sum(expanded_floor.get(candidate, False) for candidate in neighbors)
+        count_black_adjacents = sum(candidate in in_black_tiles for candidate in neighbors)
         
         # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white
         # else it remains the same (black) and needs to remain in the dictionary as black hence the `not`
-        if black:
-            new_floor[tile] = not (count_black_adjacents == 0 or count_black_adjacents > 2)
+        if tile in in_black_tiles:
+            if count_black_adjacents == 0 or count_black_adjacents > 2: # if it is in the set it means it is black
+                new_floor.remove(tile)
 
         # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
         else:
             if count_black_adjacents == 2:
-                new_floor[tile] = True
+                new_floor.add(tile)
 
     return new_floor
 
-def count_black_tiles(in_floor):
-    return sum(black for black in in_floor.values())
 
 instructions = [re.findall(re_directions, line) for line in inputs]
 
-flipped_tiles = {}
+flipped_tiles = set()
 for instruction in instructions:
     position = (0, 0, 0)
     for direction in instruction:
         position = hex_add(position, HEX_DIRECTIONS[direction])
     if position in flipped_tiles:
-        flipped_tiles[position] = not flipped_tiles[position]
+        flipped_tiles.remove(position)
     else:
-        flipped_tiles[position] = True
+        flipped_tiles.add(position)
 
-part_1 = count_black_tiles(flipped_tiles)
+part_1 = len(flipped_tiles)
 print(f'After all the flipping, {part_1} tiles are left black side up!')
 # After all the flipping, 469 tiles are left black side up!
 
@@ -72,7 +69,7 @@ current_floor = flipped_tiles.copy()
 days_of_art = 100
 for day in range(100):
     current_floor = art_installation(current_floor)
-part_2 = count_black_tiles(current_floor)
+part_2 = len(current_floor)
 
 print(f'After {days_of_art} days of art installation, there are {part_2} black tiles in the lobby!')
 # After 100 days of art installation, there are 4353 black tiles in the lobby!
