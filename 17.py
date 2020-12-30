1caac93733c5ec0d332fc99b2dbebdd7ffc32443
Maja Gacesa <maja.gacesa@gmail.com>
2020-12-30 20:49:13 +0100

    [day17] solve part 1 using sets

--- 17.py
+++ 17.py
@@ -1,39 +1,40 @@
 from collections import Counter
 import itertools
 dimension = 3
-deltas = set(itertools.product({-1,0,1}, repeat=dimension))
+DELTAS = set(itertools.product({-1,0,1}, repeat=dimension)) - {(0,0,0)}
 
-with open('inputs/17-ex') as inputfile:
-    inputs = inputfile.readlines()
 # parse input into a set
-active_cubes = set((row_num, column_num, 0)
+with open('inputs/17') as inputfile:
+    inputs = inputfile.readlines()
+active_cubes = {(row_num, column_num, 0)
                         for row_num, row in enumerate(inputs)
                         for column_num, column in enumerate(row.strip())
-                        if column == '#')
-dim_x = len(inputs[0].strip())
-dim_y = len(inputs)
-flat_dim = max(dim_x, dim_y)
-
-# domain = itertools.product(range(-flat_dim*6,flat_dim*6+1), repeat = dimension)
-# domain_purged_3d = (di for di in domain if di[2] in range(-6, 7))
+                        if column == '#'}
 
 def add_coordinates(tuple_1, tuple_2):
     return tuple(t_1 + t_2 for t_1, t_2 in zip(tuple_1,tuple_2))
 
-def count_neighbors(cube, positions):
-    out_positions = positions.copy()
-    counter = 0
-    neighbors = []
-    for delta in deltas:
-        if delta == (0, 0, 0):
-            continue
-        neighbor = add_coordinates(cube, delta)
-        neighbors.append(neighbor)
-        counter += neighbor in positions
-    if counter not in {2, 3}:
-        positions.remove(cube)
-    # check neighbors, add  if it needs to be activated
-    return #return out_positions counter
+def cube_neighbors(coordinate):
+    return {add_coordinates(coordinate, delta) for delta in DELTAS}
 
-#print(count_neighbors((1,2,0),active_cubes))
+def conway_cycle(in_active_cubes):
+    out_active_cubes = set()
+    relevant_inactive_cubes = set()
+    for cube in in_active_cubes:
+        relevant_inactive_cubes |= {neighbor for neighbor in cube_neighbors(cube) if neighbor not in in_active_cubes}
 
+    for cube in in_active_cubes | relevant_inactive_cubes:
+        neighbors = cube_neighbors(cube)
+        count_active_neighbors = sum(neighbor in in_active_cubes for neighbor in neighbors)
+        if (cube in in_active_cubes and count_active_neighbors in {2,3}) or (cube in relevant_inactive_cubes and count_active_neighbors == 3):
+            out_active_cubes.add(cube)
+
+    return out_active_cubes
+
+cycles = 6
+for _ in range(cycles):
+    active_cubes = conway_cycle(active_cubes)
+party_1 = len(active_cubes)
+
+print(f'The number of active cubes after {cycles} cycles is {party_1}!')
+# The number of active cubes after 6 cycles is 346!
